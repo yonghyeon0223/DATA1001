10398fdb3ae78233cedfe261b9795045f7d9e92c
Isaac Kim <80090053+yonghyeon0223@users.noreply.github.com>
2022-04-09 20:10:07 +1000

    Delete DATA1001 directory

--- DATA1001/README.md
+++
@@ -1,3 +0,0 @@
-# DATA1001
-
-In this page, we will post all the files related to DATA1001 project.
--- DATA1001/project2/r_practice/read_write_csv.R
+++
@@ -1,4 +0,0 @@
-library(googlesheets4)
-url <- "https://docs.google.com/spreadsheets/d/1JGMBBDOkDFzFrTvwRGJ43rXYGYUKbJXn6SZjeau4NRA/edit?resourcekey#gid=946826064"
-survey_data <- read_sheet(url)
-survey_data
